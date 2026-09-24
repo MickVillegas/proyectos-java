71e89e20b134d533b258bb71e0fb76e5f07b7164
MickVillegas <mickprofesional@gmail.com>
2026-09-24 18:29:48 +0200

    Add files via upload

---
+++ accesoDatos/src/xml/TarantulasMain.java
@@ -0,0 +1,80 @@
+package xml;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import javax.xml.transform.OutputKeys;
+
+public class TarantulasMain {
+
+	public static void main(String[] args) {
+
+		try {
+			  DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			  DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			  Document document = dBuilder.newDocument();
+			  
+			  String [] nombreComun = {"Tarántula Goliat", "Tarántula Azul de Cobalto", "Tarántula Rodillas Doradas de Chaco", "Tarántula Patas Rosas de Avicularia"};
+			  String [] nombreCientifico = {"Theraphosa blondi", "Cyriopagopus lividus", "Grammostola pulchripes", "Avicularia avicularia"};
+			  String [] nivelAgresividad = {"Alta / Defensiva", "Muy alta", "Muy baja / Docil", "Baja"};
+			  String [] colorPiel = {"Marrón oscuro o castaño uniforme con tonos rojizos en los pelos de sus patas", "Azul cobalto brillante e intenso en las patas y patas traseras, con un cefalotórax gris plateado", "Negro/marrón oscuro con franjas amarillas o doradas muy marcadas en las articulaciones de las patas", "Cuerpo negro verdoso o metálico con la punta de los dedos de las patas en tonos rosa brillante o anaranjado"};
+			  String [] habitat = {"Bosques y selvas tropicales de alta humedad en América del Sur", "Selvas tropicales de Asia", "Pastizales, pampas y zonas semiáridas de Paraguay, Argentina y Brasil", "Selvas tropicales del norte de Sudamérica y el Caribe"};
+			  
+			  Element raiz = document.createElement("tarantulas");
+			  
+			  document.appendChild(raiz);
+			  
+			  for (int i = 0; i < habitat.length; i++) {
+				  
+				  Element tarantulaElement = document.createElement("tarantula");
+				  
+				  Element nombrecom = document.createElement("NombreComun");
+				  nombrecom.appendChild(document.createTextNode(nombreComun[i]));
+				  
+				  Element nomCie = document.createElement("NombreCientifico");
+				  nomCie.appendChild(document.createTextNode(nombreCientifico[i]));
+				  
+				  Element nivAgr = document.createElement("NivelDeAgresividad");
+				  nivAgr.appendChild(document.createTextNode(nivelAgresividad[i]));
+				  
+				  Element colPi = document.createElement("ColoresDePiel");
+				  colPi.appendChild(document.createTextNode(colorPiel[i]));
+				  
+				  Element hab = document.createElement("Habitat");
+				  hab.appendChild(document.createTextNode(habitat[i]));
+				  
+				  tarantulaElement.appendChild(nombrecom);
+				  tarantulaElement.appendChild(nomCie);
+				  tarantulaElement.appendChild(nivAgr);
+				  tarantulaElement.appendChild(colPi);
+				  tarantulaElement.appendChild(hab);
+				  
+				  raiz.appendChild(tarantulaElement);
+				  
+			  }
+			  
+			  TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			  Transformer transformer = transformerFactory.newTransformer();
+			  
+			  transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			  transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			  
+			  DOMSource source = new DOMSource(document);
+			  StreamResult result = new StreamResult(new File("/home/cibertard/Escritorio/tarantulas.xml"));
+
+			  transformer.transform(source, result);
+			  
+			  
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
